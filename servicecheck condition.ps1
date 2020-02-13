02fc42038e4bbca74a62c43baeb14abc287f1f36
radhakilaru <iamradha.kilaru@gmail.com>
2020-02-13 10:41:53 -0600

    Update servicecheck condition.ps1

--- servicecheck condition.ps1
+++ servicecheck condition.ps1
@@ -1,6 +1,6 @@
-$dc = "azhdsMR765D1509"
-$pw = "Landmark1" | ConvertTo-SecureString -asPlainText -Force
-$usr = "$dc\cfe"
-$pc = "10.133.44.94" # Specify the computer that should be joined to the domain.
+$dc = ""
+$pw = "" | ConvertTo-SecureString -asPlainText -Force
+$usr = ""
+$pc = "" # Specify the computer that should be joined to the domain.
 $creds = New-Object System.Management.Automation.PSCredential($usr,$pw)
 Add-Computer -ComputerName $pc -LocalCredential $pc\admin -DomainName $dc -Credential $creds -Verbose -Restart -Force
